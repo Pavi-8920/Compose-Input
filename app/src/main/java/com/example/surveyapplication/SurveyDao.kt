2cbadd513a453d8c2162c54be4dc07d461546fed
ANJUKA G <anjukarenya23399@gmail.com>
2023-05-06 14:16:37 +0530

    Initial commit

---
+++ app/src/main/java/com/example/surveyapplication/SurveyDao.kt
@@ -0,0 +1,24 @@
+package com.example.surveyapplication
+
+/**
+ * @author CaptShiva007
+ *
+ *@author Admin
+ */
+import androidx.room.*
+
+@Dao
+interface SurveyDao {
+
+    @Query("SELECT * FROM survey_table WHERE age = :age")
+    suspend fun getUserByAge(age: String): Survey?
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertSurvey(survey: Survey)
+
+    @Update
+    suspend fun updateSurvey(survey: Survey)
+
+    @Delete
+    suspend fun deleteSurvey(survey: Survey)
+}
